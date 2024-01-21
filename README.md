e209b0cb58810dfc92878c5e4017307bf5ecdc75
herminesymphony <hermine.lalvee@symphony.com>
2024-01-21 22:44:00 +0100

    Remove generated files and make script more generic

---
+++ README.md
@@ -0,0 +1,3 @@
+This is determining the list of ghost tenant ids. it means that we have data in bigtable but the tenant does not exist. 
+
+run `./tenant_list.sh s002 1705861227` in order to run against sdu s002 and extract 1705861227
--- tenant_list.sh
+++ tenant_list.sh
@@ -1,30 +1,33 @@
 #!/bin/zsh
+ENV=$1
+SDU=$2
+LINK=${ENV}-use4-sbe-$SDU-tmp/bt/count/final/$3
+rm -rf ./processed
+mkdir processed
 
-LINK=qa-use4-sbe-s002-tmp/bt/count/final/1705591002
-WORKDIR=qa/sbe-s003
-SDU=$(echo $WORKDIR | cut -d'/' -f2)
+WORKDIR=processed/
 
 # First we download list from the Google Storage Bucket in source folder
 #echo "Downloading files"
-#mkdir -p $WORKDIR/source
-#gsutil -m cp -r "gs://$LINK" $WORKDIR/source/.
+mkdir -p $WORKDIR/source
+mkdir -p $WORKDIR/computed
+gsutil -m cp -r "gs://$LINK" $WORKDIR/source/.
 
 # Running pods example
-# WORKDIR=qa/sbe-s002 && SDU=$(echo $WORKDIR | cut -d'/' -f2)
-# kubectl get sbedata -n sbe-s001 | cut -d " " -f1 | cut -d "-" -f2 > qa/sbe-s002/running.txt
+kubectl get sbedata -n sbe-$SDU | cut -d " " -f1 | cut -d "-" -f2 > ${WORKDIR}/running.txt
 
 
-rm -rf ./$WORKDIR/processed
-mkdir $WORKDIR/processed
 
 # List all tenants from each CSV file from Dataflow count
-for f in $(find $WORKDIR/source -name \*.csv);do; cat $f | cut -d',' -f1 >> $WORKDIR/processed/created.txt;done
+for f in $(find $WORKDIR/source -name \*.csv);do; cat $f | cut -d',' -f1 >> $WORKDIR/computed/in_bt.txt;done
 # Filter negative (Bug?) and sort (deduplicate) tenantID
-cat $WORKDIR/processed/created.txt | sort -u | grep -v '-' > $WORKDIR/processed/sorted.txt
+cat $WORKDIR/computed/in_bt.txt | sort -u | grep -v '-' > $WORKDIR/computed/in_bt_sorted.txt
 # Remove running Tenants
-grep -v -x -f $WORKDIR/running.txt $WORKDIR/processed/sorted.txt > $WORKDIR/processed/with_tlm.txt
-# Remove TLM reserved Tenants
-grep -v -x -f $WORKDIR/../tlm_tenants.csv $WORKDIR/processed/with_tlm.txt > $WORKDIR/processed/to_delete.txt
+grep -v -x -f $WORKDIR/running.txt $WORKDIR/computed/in_bt_sorted.txt > $WORKDIR/computed/to_delete.txt
 
 # Prepare ids for tenant IDs list
-for t in $(cat $WORKDIR/processed/to_delete.txt); do echo "    - id: \"$t\""; done > $WORKDIR/processed/ids.txt
+for t in $(cat $WORKDIR/computed/to_delete.txt); do echo "    - id: \"$t\""; done > $WORKDIR/computed/ids.txt
+
+
+# cat output
+cat $WORKDIR/computed/ids.txt
